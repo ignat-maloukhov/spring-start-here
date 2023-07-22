8299a5f76bc2ac198b7e5de7a9026a885c57dd6f
Ignat Maloukhov <cityofdrawbridges@gmail.com>
2023-07-22 15:12:27 +0300

    Starting chapter07

---
+++ chapter07/src/main/java/demo/ignat/Main.java
@@ -0,0 +1,13 @@
+package demo.ignat;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+
+    }
+}
