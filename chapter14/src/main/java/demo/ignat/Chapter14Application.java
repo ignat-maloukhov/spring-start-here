9fffc4abf0399213306cf882a852dfe3f1285c29
Ignat Maloukhov <cityofdrawbridges@gmail.com>
2023-07-23 20:24:53 +0300

    Starting Chapter14

---
+++ chapter14/src/main/java/demo/ignat/Chapter14Application.java
@@ -0,0 +1,13 @@
+package demo.ignat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter14Application.class, args);
+	}
+
+}
---
+++ chapter14/src/test/java/demo/ignat/Chapter14ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ignat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
